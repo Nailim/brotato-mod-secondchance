af77badb6f5ff1147e5194dd133f44927de3268e
Nailim <jernej.kranjec@gmail.com>
2023-06-15 17:20:25 +0200

    Speed up text animation when using second chance

--- root/mods-unpacked/Nailim-SecondChance/extensions/ui/menus/ingame/wave_cleared_label.gd
+++ root/mods-unpacked/Nailim-SecondChance/extensions/ui/menus/ingame/wave_cleared_label.gd
@@ -6,5 +6,8 @@ func start(_is_last_wave:bool = false, _is_run_lost:bool = false, _is_run_won:bo
 	if RunData.did_use_second_chance:
 		text = tr("SECOND_CHANCE") + " (" + str(RunData.second_chance_use_count) + ")"
 		add_font_override("font", font_end_run)
+		
+		# speed up text animation, so it is visible longer
+		$Timer.wait_time = 0.05
 	
 	.start(false, false, false)
