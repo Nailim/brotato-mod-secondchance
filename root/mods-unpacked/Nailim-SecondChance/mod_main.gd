423cd2d9034291091428843111146e0d6bd1c2fa
Nailim <jernej.kranjec@gmail.com>
2024-01-14 10:04:43 +0100

    Update mode to make compatible with v1.0.1.3 release
    Same code as before, just reworked to work with the new version of the ModLoader (6.2.0) that came with the 1.0.1.3 release of the game.

--- root/mods-unpacked/Nailim-SecondChance/mod_main.gd
+++ root/mods-unpacked/Nailim-SecondChance/mod_main.gd
@@ -2,47 +2,43 @@ extends Node
 
 const SECONDCHANCE_LOG = "Nailim-SecondChance"
 
-const MOD_DIR = "Nailim-SecondChance/"
+const MOD_NAME = "Nailim-SecondChance"
 
 
-var dir = ""
-var ext_dir = ""
-var trans_dir = ""
 
-
-func _init(_modLoader = ModLoader):
+func _init() -> void:
 	ModLoaderLog.info("Init", SECONDCHANCE_LOG)
 	
-	dir = ModLoaderMod.get_unpacked_dir() + MOD_DIR
-	ext_dir = dir + "extensions/"
-	trans_dir = dir + "translations/"
+	var mod_dir = ModLoaderMod.get_unpacked_dir().plus_file(MOD_NAME)
+	var ext_dir = mod_dir.plus_file("extensions")
+	var trans_dir = mod_dir.plus_file("translations")
 	
 	
 	# Add extensions
-	ModLoaderMod.install_script_extension(ext_dir + "main.gd")
-	ModLoaderMod.install_script_extension(ext_dir + "singletons/run_data.gd")
-	ModLoaderMod.install_script_extension(ext_dir + "ui/menus/ingame/wave_cleared_label.gd")
+	ModLoaderMod.install_script_extension(ext_dir.plus_file("main.gd"))
+	ModLoaderMod.install_script_extension(ext_dir.plus_file("singletons/run_data.gd"))
+	ModLoaderMod.install_script_extension(ext_dir.plus_file("ui/menus/ingame/wave_cleared_label.gd"))
 	
-	ModLoaderMod.install_script_extension(ext_dir + "ui/menus/run/difficulty_selection/difficulty_selection.gd")
+	ModLoaderMod.install_script_extension(ext_dir.plus_file("ui/menus/run/difficulty_selection/difficulty_selection.gd"))
 	
 	
 #	# Add translations
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.en.translation")
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.fr.translation")
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.zh.translation")
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.ja.translation")
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.ko.translation")
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.zh_TW.translation")
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.ru.translation")
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.pl.translation")
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.es.translation")
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.pt.translation")
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.de.translation")
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.tr.translation")
-	ModLoaderMod.add_translation(trans_dir + "mod.translations.it.translation")
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.en.translation"))
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.fr.translation"))
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.zh.translation"))
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.ja.translation"))
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.ko.translation"))
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.zh_TW.translation"))
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.ru.translation"))
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.pl.translation"))
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.es.translation"))
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.pt.translation"))
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.de.translation"))
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.tr.translation"))
+	ModLoaderMod.add_translation(trans_dir.plus_file("mod.translations.it.translation"))
 
 
-func _ready():
+func _ready() -> void:
 	var event_listener = preload("res://mods-unpacked/Nailim-SecondChance/mod_sc_main_event_listener.gd")
 	event_listener = event_listener.new()
 	event_listener.name = "SCMainEventListener"
